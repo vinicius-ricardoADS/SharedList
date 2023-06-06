980e550165393ffe3b59f572efbe61440016b131
Vinicius Noli <viniciusnoli2016@gmail.com>
2023-06-06 08:22:41 -0300

    feat: initial commit

---
+++ app/src/main/java/br/edu/ifsp/ads/sharedlist/MainActivity.kt
@@ -0,0 +1,17 @@
+package br.edu.ifsp.ads.sharedlist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.edu.ifsp.ads.sharedlist.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val amb: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(amb.root)
+    }
+}
